95a6038e060a3fcc4bfd7905687a14ec17115b1e
fareselheni <fareselheni21@gmail.com>
2022-03-24 12:43:52 +0100

    first search

--- src/views/patientboard.vue
+++ src/views/patientboard.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="py-4 container-fluid">
+  <div class="container-fluid">
+    <div class="row mb-4">
+      <div class="col-lg-12 position-relative z-index-2">
+        <Form role="form" @submit="handleRegister">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4 py-4">
+              <vmd-input style="background-color: white" label="Search here" />
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4 py-4">
+              <div>
+                <Field
+                  name="gouvernorat"
+                  as="select"
+                  class="form-select text-center"
+                >
+                  <option value="" disabled>gouvernorat</option>
+                  <option
+                    v-for="gouv in doctors"
+                    :key="gouv._id"
+                    :value="gouv.firstname"
+                  >
+                    {{ gouv.firstname }}
+                  </option>
+                </Field>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4 py-4">
+              <div>
+                <Field
+                  name="gouvernorat"
+                  as="select"
+                  class="form-select text-center"
+                >
+                  <option value="" disabled>gouvernorat</option>
+                  <option
+                    v-for="gouv in doctors"
+                    :key="gouv._id"
+                    :value="gouv.firstname"
+                  >
+                    {{ gouv.firstname }}
+                  </option>
+                </Field>
+              </div>
+            </div>
+          </div>
+        </Form>
+      </div>
+    </div>
+  </div>
+  <div class="py-2 container-fluid">
     <div class="row mb-4">
       <div class="col-lg-12 position-relative z-index-2">
         <div class="row">
           <div
             v-for="doc in doctors"
             :key="doc._id"
-            class="col-lg-6 col-md-6 col-sm-6 mt-lg-0 mt-4 py-4"
+            class="col-lg-4 col-md-6 col-sm-6 mt-lg-0 mt-4 py-4"
           >
             <mini-cards
               :title="`${doc.gouvernorat}`"
@@ -27,10 +76,15 @@
 <script>
 import MiniCards from "./components/DoctorMiniCards.vue";
 import SearchDoctor from "../services/searchdoctor.service";
+import VmdInput from "@/components/VmdInput.vue";
+import { Form, Field } from "vee-validate";
 export default {
   name: "patientboard",
   components: {
     MiniCards,
+    VmdInput,
+    Form,
+    Field,
   },
   data() {
     return {
